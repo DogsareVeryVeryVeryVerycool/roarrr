83464b8924fbf5ec6a8727fe925a55d07493407e
DogsareVeryVeryVeryVerycool <111755061+DogsareVeryVeryVeryVerycool@users.noreply.github.com>
2023-06-15 08:16:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById("result_confidence").innerHTML = 'Accuracy - '+ (results[0].confidence*100).toFixed(2)+" %";
+document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+img.src = 'aliens-01.png';
+img1.src = 'aliens-02.png';
+img2.src = 'aliens-03.png';
+img3.src = 'aliens-04.png';
